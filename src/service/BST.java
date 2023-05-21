13528af361aa53043f94435615ae24848f4230fd
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-21 17:35:31 +0600

    implementing 'put' method finally

--- src/service/BST.java
+++ src/service/BST.java
@@ -29,9 +29,21 @@ public class BST<K extends Comparable<K>, V> {
     }
 
     private Node<K, V> put(Node<K, V> node, K key, V val) {
+
         if (node == null) {
             size++;
             return new Node<>(key, val);
         }
+
+        int comparison = key.compareTo(node.key);
+
+        if (comparison < 0)
+            node.left = put(node.left, key, val);
+        else if (comparison > 0)
+            node.right = put(node.right, key, val);
+        else
+            node.val = val;
+
+        return node;
     }
 }
