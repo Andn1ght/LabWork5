c7fbc2dd619a752524604c9cfc234747332d7d0c
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-21 16:15:29 +0600

    declaring body of subclass - 'Node'

--- src/BST.java
+++ src/BST.java
@@ -1,4 +1,11 @@
 public class BST<K extends Comparable<K>, V> {
     private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
