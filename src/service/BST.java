ff1075b83d6bc0dda7eaf7755432b04b23e6ea1d
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-21 17:31:39 +0600

    implementing 'put' method

--- src/service/BST.java
+++ src/service/BST.java
@@ -1,14 +1,28 @@
 package service;
 
 public class BST<K extends Comparable<K>, V> {
-    private class Node {
+    private class Node<K, V> {
         private K key;
         private V val;
-        private Node left, right;
+        private Node<K, V> left, right;
         public Node(K key, V val) {
             this.key = key;
             this.val = val;
         }
     }
-    private Node root;
+    private Node<K, V> root;
+    private int size;
+
+    public void put(K key, V val) {
+        if (key == null)
+            throw new IllegalArgumentException("Null key is not allowed.");
+        root = put(root, key, val);
+    }
+
+    private Node<K, V> put(Node<K, V> node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node<>(key, val);
+        }
+    }
 }
