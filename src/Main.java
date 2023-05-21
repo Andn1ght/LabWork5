a4f7d9cf68e0737de30b4f82fd7642ec64aead83
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-21 18:42:43 +0600

    testing code

--- src/Main.java
+++ src/Main.java
@@ -15,20 +15,34 @@ public class Main {
             tree.put(-4, "Zhanibek");
             tree.put(0, "Alikhan");
 
+            System.out.println("----------------------------------------------------------");
+
             for (BST.Node<Integer, String> node : tree) {
                 System.out.println("Key is " + node.getKey() + " and Value is " + node.getValue());
             }
+
+            System.out.println("----------------------------------------------------------");
 
             // Access an invalid key
             String value = tree.get(10);
             System.out.println("Value for key 10: " + value);
 
+            System.out.println("----------------------------------------------------------");
+
             System.out.println("Size of BST: " + tree.size());
+
+            System.out.println("----------------------------------------------------------");
 
             System.out.println("Deleting 8th key...");
             tree.delete(8);
 
             System.out.println("Value for key 8: " + tree.get(8));
+
+            System.out.println("----------------------------------------------------------");
+
+            for (BST.Node<Integer, String> node : tree) {
+                System.out.println("Key is " + node.getKey() + " and Value is " + node.getValue());
+            }
 
         } catch (IllegalArgumentException | NoSuchElementException e) {
             System.out.println("Error: " + e.getMessage());
